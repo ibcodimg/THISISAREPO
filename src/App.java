91dfce4d8fe24174fa63c268601a1d4b06171542
Bill Ma <billma@Mac.lan>
2026-02-01 20:20:20 -0800

    yea

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number you wish to add: ");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter the second number you wish to add: ");
+        int num2 = scanner.nextInt();
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + (num1 + num2));
     }
 }
